dd819d13b794b5c52f1a7b8b41a7b3f73f9f6a1e
DanielHernandez37 <710113@student.dusd.net>
2023-05-22 21:58:29 -0700

    Added sample spawn manager.
    Place holder spawn manager for now until better one is constructed.

--- P4TheBoysRamirez/Assets/Script/GameManager.cs
+++ P4TheBoysRamirez/Assets/Script/GameManager.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] zombiePrefabs;
+    private float spwanRangeX = 20;
+    private float spwanRangeZ = 20;
+    private float rightSpawnX = 25;
+    private float rightSpawnZS = 15.0f;
+    private float rightSpawnZE = 2.0f;
+
+
     void Start()
     {
-        
+        InvokeRepeating("SpanRandomZombie", 2 , 2f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+
     }
-}
+    private void SpanRandomZombie()
+    {
+        
+        int zombieIndex = Random.Range(0, zombiePrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spwanRangeX, spwanRangeX), 0, spwanRangeZ);
+        Instantiate(zombiePrefabs[zombieIndex], spawnPos, zombiePrefabs[zombieIndex].transform.rotation);
+        
+        Vector3 spawnPosRight = new Vector3(-rightSpawnX, 0, Random.Range(rightSpawnZS, rightSpawnZE));
+        Instantiate(zombiePrefabs[zombieIndex], spawnPosRight, zombiePrefabs[zombieIndex].transform.rotation = Quaternion.Euler(0,90,0));
+        
+        zombiePrefabs[zombieIndex].transform.rotation = Quaternion.Euler(0, 270, 0);
+
+        Vector3 spawnPosleft = new Vector3(rightSpawnX, 0, Random.Range(rightSpawnZS, rightSpawnZE));
+        Instantiate(zombiePrefabs[zombieIndex], spawnPosleft, zombiePrefabs[zombieIndex].transform.rotation);
+       
+        zombiePrefabs[zombieIndex].transform.rotation = Quaternion.Euler(0, 180, 0);
+    }
+} 
